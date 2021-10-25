29a6b878e3fe5be62f766bde7d76898fc613fd6b
Moki <mokiong1427@gmail.com>
2021-10-25 18:27:02 +0800

    Save data on mongodb database

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,8 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
 
@@ -6,7 +10,15 @@ function handler(req, res) {
             return res.status(422).json({ message: 'Invalid email address' });
         }
 
-        console.log(userEmail);
+        const client = await MongoClient.connect(
+            `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.an94b.mongodb.net/test?retryWrites=true&w=majority`
+        );
+        const db = client.db();
+
+        await db.collection('emails').insertOne({ email: userEmail });
+
+        client.close();
+
         res.status(201).json({ message: 'Signed up!' });
     }
 }
